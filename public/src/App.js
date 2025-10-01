52c70a901dccb52e4188726518d4e2d39606a879
Akinbola Mubarakah Ayomide <108186137+UmmuAbdiLlah2021@users.noreply.github.com>
2025-10-01 14:05:48 +0100

    Update App.js

--- public/src/App.js
+++ public/src/App.js
@@ -1,1 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-4xl font-bold text-blue-600 text-center mt-20">
+      🚀 Tailwind + React working in the browser!
+    </div>
+  );
+}
+
+export default App;
 
